bf0afb373f89f9c747338f88fb6b8f1041c9ee0f
JustasVit <71339306+JustasVit@users.noreply.github.com>
2022-05-02 21:41:52 +0300

    Initial spring boot app

---
+++ src/main/java/lt/PriceComparisonEngine/comparer/ComparerApplication.java
@@ -0,0 +1,13 @@
+package lt.PriceComparisonEngine.comparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/PriceComparisonEngine/comparer/ComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.PriceComparisonEngine.comparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
